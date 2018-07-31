ec756050f8168384b6f9f435b6b5d3c6b7ef30e9
dmcclelland <dmcclelland@ptc.com>
2018-07-31 08:30:07 -0400

    loading indicator implemented via global component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+
+// this lets us bind to the api to update the loading graphic globally and display it in updated app.html...
 import {inject, PLATFORM} from 'aurelia-framework';
 import {WebAPI} from './web-api';
 
-// this lets us bind to the api to update the loading graphic globally and display it in updated app.html...
 @inject(WebAPI)
 export class App {
   constructor(api) {
@@ -11,8 +12,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Contacts';
     config.map([
-      {route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select'},
-      {route: 'contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), name: 'contacts'}
+      { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
     ]);
 
     this.router = router;
